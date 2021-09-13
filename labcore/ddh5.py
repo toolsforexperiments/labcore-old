3cbfe546359047856020093e6970ca578cf90e80
pfafflabatuiuc <72898175+pfafflabatuiuc@users.noreply.github.com>
2021-09-13 16:33:10 -0500

    Cleaned up the code to make it more readable. Now if I JSON file is created but the saving fails, that file will be deleted.

--- labcore/ddh5.py
+++ labcore/ddh5.py
@@ -80,14 +80,26 @@ def _save_dictionary(dict: Dict, filepath: str) -> None:
     with open(filepath, 'w') as f:
         json.dump(dict, f, indent=2, sort_keys=True)
 
+def _pickle_and_save(obj, filepath: str) -> None:
 
-def run_and_save_sweep(sweep: Sweep, data_dir: str, name: str, **saving_dictionaries) -> None:
+    try:
+        with open(filepath, 'wb') as f:
+            pickle.dump(obj, f)
+    except TypeError as pickle_error:
+        print(f'Object could not be pickled: {pickle_error.args}')
+
+
+def run_and_save_sweep(sweep: Sweep, data_dir: str, name: str, **extra_saving_items) -> None:
     """
     Iterates through a sweep, saving the data coming through it into a file called <name> at <data_dir> directory.
 
     :param sweep: Sweep object to iterate through.
-    :param data_dir: Directory of file location
-    :param name: name of the file
+    :param data_dir: Directory of file location.
+    :param name: Name of the file.
+    :param extra_saving_items: Kwargs for extra objects that should be saved. If the kwarg is a dictionary, the function
+        will try and save it as a JSON file. If the dictionary contains objects that are not JSON serializable it will
+        be pickled. Any other kind of object will be pickled too.
+
     """
     data_dict = _create_datadict_structure(sweep)
 
@@ -96,30 +108,39 @@ def run_and_save_sweep(sweep: Sweep, data_dir: str, name: str, **saving_dictiona
 
         # Saving meta-data
         dir = writer.filepath.removesuffix(writer.filename)
-        for key, value in saving_dictionaries.items():
+        for key, value in extra_saving_items.items():
+            dictionary_dir = dir + '\\' + key + '.json'
             if isinstance(value, dict):
                 try:
-                    _save_dictionary(value, dir + '\\' + key + '.json')
-                except TypeError as e:
+                    _save_dictionary(value, dictionary_dir)
+                except TypeError as error:
+                    # Delete the file created by _save_dictionary. This file does not contain the complete dictionary.
+                    if os.path.isfile(dictionary_dir):
+                        os.remove(dictionary_dir)
+
                     converted = False # Flag to see if there has been a converted ndarray.
                     for k, v in value.items():
                         if isinstance(v, np.ndarray):
                             value[k] = v.tolist()
                             converted = True
-
                     if converted:
                         try:
                             _save_dictionary(value, dir + '\\' + key + '.json')
-                        except TypeError as error:
-                            print(f'{key} has not been able to be fully save to json: {error.args}')
+                        except TypeError as e:
+
+                            if os.path.isfile(dictionary_dir):
+                                os.remove(dictionary_dir)
+
+                            print(f'{key} has not been able to save to json: {e.args}.'
+                                  f' The item will be pickled instead.')
+                            _pickle_and_save(value, dir + '\\' + key + '.pickle')
                     else:
-                        print(f'{key} has not been able to be fully save to json: {e.args}')
+                        print(f'{key} has not been able to save to json: {error.args}.'
+                              f' The item will be pickled instead.')
+                        _pickle_and_save(value, dir + '\\' + key + '.pickle')
             else:
-                try:
-                    with open( dir + '\\' + key + '.pickle', 'wb') as f:
-                        pickle.dump(value,f)
-                except erorrisimo:
-                    print(f'{type(value)} is currently not supported for storage saving.')
+                _pickle_and_save(value, dir + '\\' + key + '.pickle')
+
 
         # Save data.
         for line in sweep:
